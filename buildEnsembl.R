78a6c3670d577248212d3fb0bc20462fb3832907
Kai Guo <guokai8@gmail.com>
2023-03-02 14:13:15 +0800

    Add files via upload

---
+++ buildEnsembl.R
@@ -0,0 +1,225 @@
+#' @title build annotation from ensembl
+#' @title build annotation from ensembl
+#' @importFrom biomaRt useEnsembl listAttributes
+#' @importFrom biomaRt useDataset
+#' @importFrom biomaRt getBM
+#' @importFrom AnnotationForge makeOrgPackage
+#' @importFrom utils remove.packages
+#' @importFrom utils install.packages
+#' @importFrom stats na.omit
+#' @param host the ensemble API host,for plant you can use
+#'             plants.ensembl.org and for human
+#'             and other species you can use uswest.ensembl.org
+#' @param species the sepcies you want to search,
+#'             you can use listSpecies to get the species name
+#' @param anntype the type of function annotation(GO,KEGG,PFAM,InterPro)
+#'             you want get from ensemble
+#' @param buildall include all prossbile annoation type listed in Ensembl
+#' @param author author for the annotation package
+#' @param maintainer maintainer for the annotation package
+#' @param tax_id taxonomy id for the species
+#' @param genus genus name for the annotation package
+#' @param version version number for the annotation package
+#' @param plant plant or animal species (TRUE/FALSE)
+#' @param install install the package or not(default: TRUE)
+#' @param rebuild rebuild the package or not(default: FALSE)
+#' @param outputDir temporary output path
+#' @examples
+#' fromEnsembl(species = "Caenorhabditis elegans", anntype="GO")
+#' @author Kai Guo
+#' @return annotation package
+#' @export
+fromEnsembl <- function(species = "Caenorhabditis elegans",
+                    host = NULL,
+                    anntype = NULL, buildall = TRUE, author = NULL,
+                    maintainer = NULL, tax_id = NULL, genus = NULL,
+                    version = NULL, plant = FALSE,
+                    install = TRUE, outputDir = NULL, rebuild = FALSE){
+    if(isTRUE(plant)){
+        host = "https://plants.ensembl.org"
+        mart = useEnsembl("plants_mart", host = host)
+    }else{
+     #   mart = useEnsembl("ensembl", mirror = host)
+        mart = useEnsembl("ensembl")
+        
+    }
+    dbinfo<-.getmartdb(species, mart)
+    dbname1 <- paste0('org.', strsplit(species," ")[[1]][1], '.eg.db')
+    if(isTRUE(rebuild)){
+        suppressMessages(remove.packages(dbname1))
+    }
+    if(is_installed(dbname1)){
+        suppressMessages(requireNamespace(dbname1, quietly = TRUE))
+        cat("You alreay had the annotation package: ", dbname1, " \n")
+    }else{
+    dbname <- as.character(dbinfo$dbname)
+    dataset <- useDataset(dbname, mart = mart)
+    attr <- listAttributes(dataset)$name
+    if(is.null(anntype)){
+        if(isTRUE(buildall)){
+        anntype <- c("GO","KEGG","Reactome","PFAM","InterPro")
+    }else{
+        stop("You need to specific anntation type!\n")
+    }
+    }
+    chr_values <- as.vector(unlist(dbinfo$chr_info$name))
+    geneinfo <- getBM(attributes = c("ensembl_gene_id","description"),
+                    filters ="chromosome_name", values = chr_values, dataset)
+    # geneinfo<-geneinfo[nchar(geneinfo[,2])>1,]
+    colnames(geneinfo) <- c("GID", "GENENAME")
+    geneinfo <- na.omit(geneinfo)
+    gene2symbol <- getBM(attributes = c("ensembl_gene_id","external_gene_name"),
+            filters = "chromosome_name", values = chr_values, dataset)
+    gene2symbol <- gene2symbol[nchar(gene2symbol[,2])>1,]
+    colnames(gene2symbol) <- c("GID","SYMBOL")
+    gene2entrezid <- getBM(attributes = c("ensembl_gene_id","entrezgene_id"),
+            filters ="chromosome_name",values = chr_values, dataset)
+    gene2entrezid <- gene2entrezid[nchar(gene2entrezid[,2])>1, ]
+    colnames(gene2entrezid) <- c("GID","ENTREZID")
+    gene2entrezid <- na.omit(gene2entrezid)
+    if(("GO" %in% anntype) & sum(grepl('go_id',attr))>=1){
+        gene2go <- getBM(attributes = c("ensembl_gene_id", "go_id",
+                    "go_linkage_type"),
+                    filters ="chromosome_name", values = chr_values, dataset)
+        gene2go <- gene2go[nchar(gene2go[,2])>1, ]
+        gene2go <- gene2go[nchar(gene2go[,3])>1, ]
+        colnames(gene2go) <- c("GID", "GO", "EVIDENCE")
+        gene2go <- na.omit(gene2go)
+        gene2go <- gene2go[!duplicated(gene2go), ]
+    }else{
+        gene2go <- data.frame("GID" = geneinfo$GID,"GO" = "GO:0008150",
+        "EVIDENCE" = "IEA")
+        cat("Gene Ontology are not list in your annotation database\n")
+    }
+    if(("KEGG" %in% anntype) & (sum(grepl('kegg_enzyme',attr))>=1)){
+        gene2path <- getBM(attributes = c("ensembl_gene_id", "kegg_enzyme"),
+                    filters = "chromosome_name", values = chr_values, dataset)
+        gene2path[,2] <- sub('\\+.*', '', gene2path[,2])
+        gene2path <- gene2path[nchar(gene2path[,2])>1, ]
+        colnames(gene2path) <- c("GID", "PATH")
+        gene2path <- na.omit(gene2path)
+        gene2path <- gene2path[!duplicated(gene2path), ]
+    }else{
+        gene2path <- data.frame("GID" = geneinfo$GID,"PATH" = "01100")
+        cat("KEGG Pathway are not list in your annotation database\n")
+    }
+    if(("PFAM"%in%anntype)&(sum(grepl('pfam',attr))>=1)){
+        gene2pfam <- getBM(attributes = c("ensembl_gene_id","pfam"),
+                    filters ="chromosome_name", values = chr_values, dataset)
+        gene2pfam <- gene2pfam[nchar(gene2pfam[,2])>1, ]
+        colnames(gene2pfam) <- c("GID", "PFAM")
+        gene2pfam <- na.omit(gene2pfam)
+    }else{
+        gene2pfam <- data.frame("GID" = geneinfo$GID, "PFAM" = "PF00001")
+        cat("Protein Family are not list in your annotation database\n")
+    }
+    if(("InterPro" %in% anntype) & (sum(grepl('interpro',attr))>=1)){
+        gene2interpro <- getBM(attributes = c("ensembl_gene_id", "interpro"),
+            filters = "chromosome_name", values = chr_values, dataset)
+        gene2interpro <- gene2interpro[nchar(gene2interpro[, 2])>1, ]
+        colnames(gene2interpro) <- c("GID", "INTERPRO")
+        gene2interpro <- na.omit(gene2interpro)
+    }else{
+        gene2interpro <- data.frame("GID" = geneinfo$GID,
+            "INTREPRO" = "IPR000001")
+        cat("InterPro are not list in your annotation database\n")
+    }
+    if(("Reactome"%in%anntype) & (sum(grepl('reactome',attr))>=1)){
+        if(isTRUE(plant)){
+            gene2reactome <- getBM(attributes = c("ensembl_gene_id",
+            "plant_reactome_pathway"), filters = "chromosome_name",
+            values = chr_values, dataset)
+    }else{
+        gene2reactome <- getBM(attributes = c("ensembl_gene_id", "reactome"),
+        filters = "chromosome_name", values = chr_values, dataset)
+    }
+        gene2reactome <- gene2reactome[nchar(gene2reactome[,2])>1,]
+        colnames(gene2reactome) <- c("GID","REACTOME")
+        gene2reactome <- na.omit(gene2reactome)
+    }else{
+        gene2reactome <- data.frame("GID" = geneinfo$GID,
+        "REACTOME" = "RSA0000000")
+        cat("Reactome Pathway are not list in your annotation database\n")
+    }
+    if(is.null(author)){
+        author <-"myself"
+    }
+    if(is.null(maintainer)){
+        maintainer <- "mysel<myself@gmail.com>"
+    }
+    if(is.null(tax_id)){
+        tax_id <- "123"
+    }
+    if(is.null(version)){
+        version <- "0.0.1"
+    }
+    if(is.null(genus)){
+        genus <- ""
+    }
+    if(is.null(outputDir)){
+        outputDir <- tempdir()
+    }
+    species <- gsub(' .*', '', species)
+    package <- suppressWarnings(makeOrgPackage(gene_info = geneinfo,
+        symbol = gene2symbol,
+        entrezid = gene2entrezid,
+        go = gene2go,
+        path = gene2path,
+        pfam = gene2pfam,
+        interpro = gene2interpro,
+        reactome = gene2reactome,
+        version = version,
+        maintainer = maintainer,
+        author = author,
+        outputDir = outputDir,
+        tax_id = tax_id,
+        genus = genus,
+        species = species,
+        verbose = FALSE,
+        goTable = "go"
+    ))
+    if(isTRUE(install)){
+        install.packages(package, repos = NULL, type = "source")
+        unlink(package, recursive = TRUE)
+    }else{
+        .show.path(package)
+        .show.tables(package)
+        #return(package)
+    }
+    }
+}
+##' @title list species available in Ensembl
+##' @importFrom  biomaRt useMart
+##' @importFrom  biomaRt listDatasets
+##' @param host Ensembl host site
+##' @param plant use plant database or not (default: FALSE)
+##' @examples
+##' listSpecies()
+##' @author Kai Guo
+##' @return data.frame with species information
+##' @export
+listSpecies <- function(host = "www", plant = FALSE){
+    cat("You could choose different host to get high speed!\n")
+    if(isTRUE(plant)){
+        host <- "https://plants.ensembl.org"
+        mart <- useEnsembl("plants_mart", host = host)
+    }else{
+        cat("host: 'www', 'uswest', 'useast', 'asia'\n" )
+      #  mart <- useEnsembl("ENSEMBL_MART_ENSEMBL", mirror = host)
+        mart <- useEnsembl("ensembl")
+        
+    }
+    res <- tryCatch(
+        expr = { 
+            listDatasets(mart)
+        },
+        error = function(e){
+            NULL
+        },
+        warning = function(w){
+            NULL
+        })
+    if(is.data.frame(res)) colnames(res)[2] <- "species"
+    res
+}
+
---
+++ misc.R
@@ -0,0 +1,275 @@
+##' uppercase the first letter
+##' @param x string
+##' @return character with first letter uppercase
+##' @author Kai Guo
+simpleCap <- function(x) {
+    s <- strsplit(x, " ")[[1]]
+    paste(toupper(substring(s, 1, 1)), substring(s, 2), sep = "", 
+        collapse = " ")
+}
+
+#' extract GO information from NCBI and filter by taxid
+#' @importFrom data.table fread
+#' @importFrom R.utils gunzip
+#' @importFrom utils download.file
+#' @importFrom data.table ":="
+#' @param taxid taxonomy id for the species
+#' @param species species name(common name,kegg.species.code or scientifc name)
+#' @return dataframe with gene2go information
+#' @author Kai Guo
+.extratGO <- function(taxid = NULL, species = NULL){
+    # temp file
+    if(is.null(taxid)){
+        taxid <- .get.species.info(species)['tax.id']
+    }
+    tmp <- paste(tempfile(), "gz", sep = ".")
+    # import Gene to Gene Ontology from NCBI Gene database
+    download.file("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz",
+        quiet = TRUE,destfile = tmp)
+    # uncompress
+    gunzip(tmp)
+    # read the file (linux and windows)
+    gene2go <- fread(sub("\\.gz", "", tmp), verbose = FALSE, 
+                showProgress = FALSE)
+    # select columns and rename
+    gene2go <- unique(gene2go[, c(seq_len(4)), with = FALSE])
+    colnames(gene2go) <- c("taxid", "GID", "GO", "EVIDENCE")
+
+    # convert columns in character
+    gene2go[, `:=`(taxid = as.character(gene2go$taxid),
+            GID = as.character(gene2go$GID))]
+    # filter with taxid
+    gene2go <- as.data.frame(gene2go)
+    gene2go <- gene2go[gene2go$taxid == taxid,2:4]
+    return(gene2go)
+}
+
+
+#' @importFrom data.table fread
+#' @importFrom R.utils gunzip
+#' @importFrom utils download.file
+#' @importFrom data.table ":="
+#' @param taxid taxonomy id for the species
+#' @param species species name(common name,kegg.species.code or scientifc name)
+#' @author Kai Guo
+.extratGene <- function(taxid = NULL, species = NULL){
+    if(is.null(taxid)){
+        taxid <- .get.species.info(species)['tax.id']
+    }
+    # temp file
+    tmp <- paste(tempfile(), "gz", sep = ".")
+    # import Gene to Gene Ontology from NCBI Gene database
+    download.file("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz",
+        quiet = TRUE, destfile = tmp)
+    # uncompress
+    gunzip(tmp)
+    # read the file (linux and windows)
+    gene2info <- fread(sub("\\.gz", "", tmp), verbose=FALSE, 
+            showProgress = FALSE)
+    # select columns and rename
+    gene2info <- unique(gene2info[, c(seq_len(3), 9), with = FALSE])
+    colnames(gene2info) <- c("taxid", "GID", "SYMBOL", "GENENAME")
+    # convert columns in character
+    gene2info[, `:=`(taxid = as.character(gene2go$taxid),
+            GID = as.character(gene2go$GID))]
+    # filter with taxid
+    gene2go <- gene2go[taxid == taxid, 2:4]
+    return(as.data.frame(gene2info))
+}
+#' modified from pathview kegg.species.code
+#' @importFrom utils data
+#' @param species species name(common name,kegg.species.code or scientifc name)
+#' @param na.rm TRUE/FALSE
+#' @return character with species name
+#' @author Kai Guo
+.get.species.info <- function (species = "hsa", na.rm = FALSE){
+    nspec <- length(species)
+    if(!exists("korg")) data(korg)
+    ridx <- match(species, korg[, seq_len(5)]) %% nrow(korg)
+    nai <- is.na(ridx)
+    if (sum(nai) > 0) {
+    na.msg <- sprintf("Unknown species '%s'! in KEGG database, try others...", paste(species[nai],
+        sep = "", collapse = "', '"))
+    message("Note: ", na.msg)
+    }
+    if (sum(nai) == nspec) {
+        stop.msg <- "All species are invalid!"
+        stop(stop.msg)
+    }
+    if (any(ridx[!nai] == 0)) ridx[!nai & ridx == 0] <- nrow(korg)
+    if (isTRUE(na.rm)) ridx = ridx[!nai]
+    species.info <- korg[ridx, ]
+    return(species.info)
+}
+
+##' @title get species information in Ensembl
+##' @importFrom dplyr select_
+##' @importFrom dplyr collect
+##' @importFrom dplyr pull
+##' @importFrom magrittr %>%
+##' @importFrom biomaRt listDatasets
+##' @importFrom jsonlite fromJSON toJSON 
+##' @importFrom httr content GET
+##' @param species species
+##' @param mart biomaRt mart
+##' @return list with species information
+##' @author Kai Guo
+.getmartdb <- function(species, mart){
+    lhs <- listDatasets(mart)
+    spe <- simpleCap(species);
+    spe <- gsub(' ', '\\\\s', spe)
+    spe <- paste0(spe,'\\s','genes')
+    sel <- grepl(spe, lhs$description, ignore.case = FALSE)
+    tmp <- lhs[sel, ]
+    dataset <- tmp%>%select_(~dataset)%>%collect%>%pull(1)
+    if((length(dataset) == 0) | (length(dataset) > 1)){
+        stop("Maybe you need first check the avaliable database by
+            using listSpecies()\n")
+    }
+    chr <- tmp%>%select_(~dataset)%>%collect%>%pull(1)
+    organism <- gsub(' ', '_', sub('(_gene|_eg).*', '', chr))
+    if(organism == "Oryza_sativa_Japonica"){
+        organism = "Oryza_sativa"
+    }
+    if(mart@biomart =="plants_mart"){
+        pre_site <- "http://rest.ensembl.org/info/assembly/"
+    }else{
+        pre_site <- "http://rest.ensembl.org/info/assembly/"
+    }
+    tryCatch({
+    chr_d <- fromJSON(toJSON(content(GET(paste0(pre_site, organism,"?"), 
+            content_type("application/json")))))}, error = function(e)
+    stop(
+    "The API 'http://rest.ensembl.org' does not seem to work properly.
+    Are you connected to the internet? Is the homepage
+    'http://rest.ensembl.org' currently available? or specie wasn't included!", call. = FALSE))
+    chr_info <- chr_d$top_level_region
+    chr_version <- chr_d$assembly_name
+    chr_assembly_date <- chr_d$assembly_date
+    rhs <- list(dbname = dataset, chr_info = chr_info,
+        chr_version = chr_version, chr_assembly_date = chr_assembly_date)
+    return(rhs)
+}
+
+#'@title get database name by using species name
+#'@param species species name
+#'@return character with database name
+#'@author Kai Guo
+.getdbname <- function(species = species){
+    species = tryCatch(match.arg(species,c("anopheles",
+        "arabidopsis", "bovine", "celegans", "canine", "fly", "zebrafish",
+        "ecoli", "ecsakai", "chicken", "human", "mouse", "rhesus", "malaria",
+        "chipm", "rat",
+        "toxoplasma", "streptomyces", "pig", "yeast", "xenopus", "warm")),
+        error=function(cond){return("unsupported")})
+    if (species == "anopheles") {
+        dbname <- "org.Ag.eg.db.sqlite"
+    } else if (species == "bovine") {
+        dbname <- "org.Bt.eg.db.sqlite"
+    } else if (species == "canine") {
+        dbname <- "org.Cf.eg.db.sqlite"
+    } else if (species == "worm" || species == "celegans") {
+        dbname <- "org.Ce.eg.db.sqlite"
+    } else if (species == "chicken") {
+        dbname <- "org.Gg.eg.db.sqlite"
+    } else if (species == "ecolik12") {
+        dbname <- "org.EcK12.eg.db.sqlite"
+    } else if (species == "ecsakai") {
+        dbname <- "org.EcSakai.eg.db.sqlite"
+    } else if (species == "fly") {
+        dbname <- "org.Dm.eg.db.sqlite"
+    } else if (species == "human") {
+        dbname <- "org.Hs.eg.db.sqlite"
+    } else if (species == "chipm") {
+        dbname <- "org.Pt.eg.db.sqlite"
+    }else if (species == "mouse") {
+        dbname <- "org.Mm.eg.db.sqlite"
+    } else if (species == "pig") {
+        dbname <- "org.Ss.eg.db.sqlite"
+    } else if (species == "rat") {
+        dbname <- "org.Rn.eg.db.sqlite"
+    } else if (species == "rhesus") {
+        dbname <- "org.Mmu.eg.db.sqlite"
+    } else if (species == "xenopus") {
+        dbname <- "org.Xl.eg.db.sqlite"
+    } else if (species == "zebrafish") {
+        dbname <- "org.Dr.eg.db.sqlite"
+    } else {
+        dbname <- NULL
+    }
+    return(dbname)
+}
+##' @author Kai Guo
+readidx <- function()
+{
+    n <- readline(prompt = "Enter an index: ")
+    return(as.integer(n))
+}
+##' @title check package installed or not
+##' @param pkg package name
+##' @return TRUE/FALSE
+##' @author Kai Guo
+is_installed <- function(pkg) {
+    nzchar(system.file(package = pkg))
+}
+##' @title show the package path
+##' @param package the full path of the package
+##' @return whole path for the package
+##' @author Kai Guo
+.show.path <- function(package){
+    cat("################################################################\n")
+    cat("Please find your annotation package in ...\n")
+    cat(package, "\n")
+    cat("You can install it by using\n")
+    cat(paste0("install.packages(\"", package,'\"',",repos = NULL,type='source')"), "\n")
+    cat("################################################################\n")
+}
+##' @title show the package content
+##' @importFrom RSQLite dbConnect
+##' @importFrom RSQLite SQLite
+##' @importFrom RSQLite dbListTables
+##' @importFrom RSQLite dbReadTable
+##' @importFrom RSQLite dbDisconnect
+##' @param package the full path of the package
+##' @return vector 
+##' @author Kai Guo
+.show.tables <- function(package){
+    pkg <- basename(package)
+    path <- paste0(package, "/inst/extdata/", sub('.db', '.sqlite', pkg))
+    con <- dbConnect(SQLite(),path)
+    cat("Here are the tables in the package", pkg, "...\n")
+    dblist <- dbListTables(con)
+    cat(dblist, "\n")
+    cat("################################################################\n")
+    dbDisconnect(con)
+}
+##' @title get annotataion table from temporary package
+##' @importFrom RSQLite dbConnect
+##' @importFrom RSQLite SQLite
+##' @importFrom RSQLite dbReadTable
+##' @importFrom RSQLite dbDisconnect
+##' @importFrom dplyr left_join
+##' @param path full path for the temporary package
+##' @param table a character  indicate the table you want extract
+##' @examples
+##' data(ath)
+##' pack <- fromOwn(geneinfo = ath, install = FALSE, species ="test")
+##' # head(getTable(path = pack, table = "gene_info"))
+##' @export
+##' @return data.frame 
+##' @author Kai Guo
+
+getTable <- function(path, table = "go_all"){
+    pkg <- basename(path = path)
+    path <- paste0(path, "/inst/extdata/", sub('.db', '.sqlite', pkg))
+    con <- dbConnect(SQLite(), path)
+    gene_info <- dbReadTable(con, "gene_info")
+    anno <- dbReadTable(con, table)
+    res <- left_join(gene_info, anno, by = c("X_id" = "X_id"))
+    colnames(res)[1] <- "ID"
+    dbDisconnect(con)
+    return(res)
+}
+
+### from biomaRt
+
